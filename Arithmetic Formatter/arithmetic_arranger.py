a92037d072ad14148d6ff160ceaceccb1b9f44eb
TheWonderfulLizardOfOz <Blue.Bean.Bear.Bag@gmail.com>
2021-06-21 11:45:55 +0100

    Made an overall layout for arithmetic_arranger.py

--- Arithmetic Formatter/arithmetic_arranger.py
+++ Arithmetic Formatter/arithmetic_arranger.py
@@ -1,4 +1,31 @@
-def arithmetic_arranger(problems):
+import re
+class Arithmetic_arranger():
+    def __init__(self, problems):
+        self.problems = problems
+        self.num1 = []
+        self.num2 = []
+        self.operands = []
+        self.totla_problems = len(problems)
+        self.individual_problems()
+        self.arithmetic_arranger()
 
+    def individual_problems(self):
+        for problem in self.problems:
+            nums = self.number_finder(problem)
+            self.num1.append(nums[0])
+            self.num2.append(nums[1])
+            self.operands.append(self.operand_finder(problem))
+            print(self.num1)
+            print(self.num2)
+            print(self.operands)
+            
+    def operand_finder(self, problem):
+        operand = re.findall("[+-]", problem)
+        return operand
 
-    return arranged_problems
+    def number_finder(self, problem):
+        nums = re.findall("[0-9]+", problem)
+        return nums
+    
+    def arithmetic_arranger(self, problems):
+        return arranged_problems
