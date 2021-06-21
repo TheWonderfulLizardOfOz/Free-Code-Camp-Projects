8e6ed68392879d1ac24c1b7235866452a542948b
TheWonderfulLizardOfOz <Blue.Bean.Bear.Bag@gmail.com>
2021-06-21 13:53:00 +0100

    Did mor progress on project, need to fix a bug

--- Arithmetic Formatter/arithmetic_arranger.py
+++ Arithmetic Formatter/arithmetic_arranger.py
@@ -5,27 +5,56 @@ class Arithmetic_arranger():
         self.num1 = []
         self.num2 = []
         self.operands = []
-        self.totla_problems = len(problems)
-        self.individual_problems()
-        self.arithmetic_arranger()
+        self.totalProblems = len(problems)
+        self.topLine = " "
+        self.bottomLine = ""
+        self.individualProblems()
+        self.arithmeticArranger()
 
-    def individual_problems(self):
+    def individualProblems(self):
         for problem in self.problems:
-            nums = self.number_finder(problem)
+            nums = self.numberFinder(problem)
             self.num1.append(nums[0])
             self.num2.append(nums[1])
-            self.operands.append(self.operand_finder(problem))
+            self.operands.append(self.operandFinder(problem))
             print(self.num1)
             print(self.num2)
             print(self.operands)
             
-    def operand_finder(self, problem):
+    def operandFinder(self, problem):
         operand = re.findall("[+-]", problem)
         return operand
 
-    def number_finder(self, problem):
+    def numberFinder(self, problem):
         nums = re.findall("[0-9]+", problem)
         return nums
-    
-    def arithmetic_arranger(self, problems):
-        return arranged_problems
+
+    def findBiggestLength(self, num1, num2):
+        if len(num1) > len(num2):
+            return len(num1)
+        else:
+            return len(num2)
+
+    def arrangeTop(self, num1, totalLength, biggestLength):
+        if len(num1) == biggestLength:
+            self.topLine += " " + " " + str(num1) + " "
+        else:
+            totalToAdd = totalLength - len(num1)
+            self.topLine += " "*totalToAdd
+            self.topLine += str(num1) + " "
+
+    def arrangeBottom(self, num2, totalLength, biggestLength):
+        self.bottomLine += "+ "
+        if len(num2) != biggestLength:
+            totalToAdd = totalLength - len(num2) - 2
+            self.bottomLine += " "*totalToAdd
+        self.bottomLine += str(num2) + " "
+            
+    def arithmeticArranger(self):
+        for i in range(self.totalProblems):
+            biggestLength = self.findBiggestLength(self.num1[i], self.num1[i])
+            totalLength = biggestLength + 2
+            self.arrangeTop(self.num1[i], totalLength, biggestLength)
+            self.arrangeBottom(self.num2[i], totalLength, biggestLength)
+        print(self.topLine)
+        print(self.bottomLine)
